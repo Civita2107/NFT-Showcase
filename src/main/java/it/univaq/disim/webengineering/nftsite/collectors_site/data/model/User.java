17cecc8f93677cd09dc838d19cbe951b7724eed2
Civita2107 <f.civitareale@gmail.com>
2023-01-30 16:03:05 +0100

    Correzioni

---
+++ src/main/java/it/univaq/disim/webengineering/nftsite/collectors_site/data/model/User.java
@@ -0,0 +1,19 @@
+package it.univaq.disim.webengineering.nftsite.collectors_site.data.model;
+import it.univaq.disim.webengineering.nftsite.framework.data.DataItem;
+
+
+public interface User extends DataItem<Integer>  {
+
+    int getId();
+    
+    void setId(int Id);
+    
+    String getUsername();
+    
+    void setUsername(String username);
+
+    String getEmail();
+    
+    void setEmail(String email);
+    
+}
