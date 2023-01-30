6d5ef45207e95998b8ed5eac0419b73f02510f34
Bepsommer <gtgiorgiot@gmail.com>
2023-01-30 18:20:43 +0100

    alcune classi dao

--- src/main/java/it/univaq/disim/webengineering/nftsite/collectors_site/data/DAO/UserDAO.java
+++ src/main/java/it/univaq/disim/webengineering/nftsite/collectors_site/data/DAO/UserDAO.java
@@ -6,12 +6,27 @@ import it.univaq.disim.webengineering.nftsite.collectors_site.data.model.Nft;
 import it.univaq.disim.webengineering.nftsite.collectors_site.data.model.User;
 import it.univaq.disim.webengineering.nftsite.framework.data.DataLayerException;
 
+import javax.servlet.http.Part;
+import java.io.File;
+import java.io.IOException;
+import java.security.NoSuchAlgorithmException;
+
 public interface UserDAO {
-    
-    void storeUser(User user) throws  DataLayerException;
-    
+
+    boolean getUsernameEsistente(String username) throws DataLayerException;
+
+    boolean getEmailEsistente(String email) throws DataLayerException;
+
+    User getCredenziali(String username, String password) throws DataLayerException;
+
+    User nuovoUtente(User user) throws DataLayerException;
+
     User getUser(int id) throws DataLayerException;
 
-   // List<Nft> getNfts(User user) throws DataLayerException;
+    int updateUser(User user) throws DataLayerException;
+
+    int updateUserPassword(User user) throws DataLayerException;
+
+    void storeUser(User user) throws  DataLayerException;
 
 }
