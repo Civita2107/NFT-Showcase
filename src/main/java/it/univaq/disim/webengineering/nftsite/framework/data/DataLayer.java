5813b1497c98ce7f8f5a11b0e3dcf0cc555049f8
Civita2107 <f.civitareale@gmail.com>
2023-01-30 17:30:05 +0100

    fsd

---
+++ src/main/java/it/univaq/disim/webengineering/nftsite/framework/data/DataLayer.java
@@ -0,0 +1,66 @@
+package it.univaq.disim.webengineering.nftsite.framework.data;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.util.HashMap;
+import java.util.Map;
+import javax.sql.DataSource;
+
+public class DataLayer {
+
+    private final DataSource datasource;
+    private Connection connection;
+    private final Map<Class,DAO> daos;
+    private final DataCache cache;
+
+    public DataLayer(DataSource datasource) throws SQLException {
+        super();
+        this.datasource = datasource;
+        this.connection = datasource.getConnection();
+        this.daos = new HashMap<>();
+        this.cache = new DataCache();
+    }
+
+    public void registerDAO(Class entityClass, DAO dao) throws DataException {
+        daos.put(entityClass, dao);
+        dao.init();
+    }
+
+    public DAO getDAO(Class entityClass, DAO dao) throws DataException {
+        return daos.get(entityClass);
+    }
+
+    public void init() throws DataException {
+        // call register for your own DAOs
+    }
+
+    public void destroy() {
+        try {
+            if (connection != null) {
+                connection.close();
+                connection = null;
+            }
+        } catch (SQLException ex) {
+            //
+        }
+    }
+
+    public DataSource getDatasource() {
+        return datasource;
+    }
+
+    public Connection getConnection() {
+        return connection;
+    }
+
+    public DataCache getCache() {
+        return cache;
+    }
+
+    @Override
+    public void close() throws Exception {
+        destroy();
+    }
+
+    
+}
