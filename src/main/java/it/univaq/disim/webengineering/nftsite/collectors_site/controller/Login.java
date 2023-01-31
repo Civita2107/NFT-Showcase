d66d6a5cbadea3f4b954622aa323ef76ea228391
Civita2107 <f.civitareale@gmail.com>
2023-01-31 19:28:01 +0100

    Implementati metodi

--- src/main/java/it/univaq/disim/webengineering/nftsite/collectors_site/controller/Login.java
+++ src/main/java/it/univaq/disim/webengineering/nftsite/collectors_site/controller/Login.java
@@ -7,11 +7,20 @@ import javax.servlet.http.*;
 
 public class Login {
     
-    private void actionb_default(HttpServletRequest request, HttpServletResponse response) throws IOException, TemplateManagerException {
+    private void action_default(HttpServletRequest request, HttpServletResponse response) throws IOException, TemplateManagerException {
         TemplateResult result = new TemplateResult(getServletContext());
 
         request.setAttribute("referrer", request.getParameter("referrer"));
-        request.setAttribute("outline", result);
+     //   request.setAttribute("outline", result);
         result.activate("login.ftl", request, response);
+    }
+
+    private void action_login(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        String username = request.getParameter("user");
+        String password = request.getParameter("password");
+
+        if (username.isBlank() && password.isBlank()) {
+            request.setAttribute("error", "Inserire username e/o password");
+        }
     }
 }
