ef487fbb1f7db386570fad35264f833e060317b1
Civita2107 <f.civitareale@gmail.com>
2023-01-31 19:03:45 +0100

    fec

--- src/main/java/it/univaq/disim/webengineering/nftsite/collectors_site/controller/Login.java
+++ src/main/java/it/univaq/disim/webengineering/nftsite/collectors_site/controller/Login.java
@@ -1,9 +1,17 @@
 package it.univaq.disim.webengineering.nftsite.collectors_site.controller;
 
+import java.io.IOException;
+
 import javax.servlet.*;
-import javax.servlet.http;
+import javax.servlet.http.*;
 
 public class Login {
     
-    private void actionb_default(HttpServletRequest request, HttpServletResponse)
+    private void actionb_default(HttpServletRequest request, HttpServletResponse response) throws IOException, TemplateManagerException {
+        TemplateResult result = new TemplateResult(getServletContext());
+
+        request.setAttribute("referrer", request.getParameter("referrer"));
+        request.setAttribute("outline", result);
+        result.activate("login.ftl", request, response);
+    }
 }
