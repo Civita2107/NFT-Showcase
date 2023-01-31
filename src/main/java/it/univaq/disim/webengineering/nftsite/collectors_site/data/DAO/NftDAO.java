aa839bb1a6bcf90a67ea44e54077f416e2bb4bf3
Civita2107 <f.civitareale@gmail.com>
2023-01-31 18:37:03 +0100

    vrfc

--- src/main/java/it/univaq/disim/webengineering/nftsite/collectors_site/data/DAO/NftDAO.java
+++ src/main/java/it/univaq/disim/webengineering/nftsite/collectors_site/data/DAO/NftDAO.java
@@ -7,12 +7,6 @@ import it.univaq.disim.webengineering.nftsite.collectors_site.data.model.Collect
 import it.univaq.disim.webengineering.nftsite.collectors_site.data.model.Nft;
 import it.univaq.disim.webengineering.nftsite.framework.data.DataLayerException;
 
-import java.security.NoSuchAlgorithmException;
-import java.sql.Connection;
-import java.sql.SQLException;
-import java.util.List;
-
-
 public interface NftDAO {
 
     void storeNft(Nft nft) throws DataLayerException;
@@ -20,19 +14,5 @@ public interface NftDAO {
     Nft getNft(int id) throws DataLayerException;
 
     Collection getCollection(Collection collection) throws DataLayerException;
-
-    List<Nft> searchNftByStrings(String value, Integer numberOfElements, Integer id) throws DataLayerException;
-
-    Nft getNftById(int id) throws DataLayerException, SQLException, NoSuchAlgorithmException;
-
-    List<Nft> getNftByCollezione(int id, Integer numberOfElements) throws DataLayerException;
-
-    List<Nft> getNftByCollezione(int id) throws DataLayerException;
-
-    Nft getNftByWallet(int id, int idWallet) throws DataLayerException;
-
-    void insertNft(Nft nft, String[] id, int idWallet) throws DataLayerException, NoSuchAlgorithmException;
-
-    boolean deleteNft(int id) throws DataLayerException;
-
+    
 }
