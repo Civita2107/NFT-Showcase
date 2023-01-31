bc9082803c509793e4b4a18a68a6a26c6e6e00f8
Civita2107 <f.civitareale@gmail.com>
2023-01-31 19:27:38 +0100

    ecd

--- src/main/java/it/univaq/disim/webengineering/nftsite/collectors_site/controller/Error.java
+++ src/main/java/it/univaq/disim/webengineering/nftsite/collectors_site/controller/Error.java
@@ -1,5 +1,16 @@
 package it.univaq.disim.webengineering.nftsite.collectors_site.controller;
 
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
 public class Error {
+
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response) {
+        try {
+            String https_redirect_url = SecurityHelpers(request);
+            request.setAttribute("https_redirect", https_redirect_url);
+        }
+    }
     
 }
