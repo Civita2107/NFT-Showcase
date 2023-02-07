d8163d69ec3715db9a6c6e11d413b2fabe589285
Bepsommer <gtgiorgiot@gmail.com>
2023-02-07 18:09:51 +0100

    classe utiity quasi implementata

--- src/main/java/it/univaq/disim/webengineering/nftsite/collectors_site/controller/Utility.java
+++ src/main/java/it/univaq/disim/webengineering/nftsite/collectors_site/controller/Utility.java
@@ -9,9 +9,13 @@ import javax.servlet.http.HttpSession;
 
 import org.asynchttpclient.AsyncHttpClient;
 
+import it.univaq.disim.webengineering.nftsite.collectors_site.data.DAO.CollectionDAO;
+import it.univaq.disim.webengineering.nftsite.collectors_site.data.DAO.NftDAO;
 import it.univaq.disim.webengineering.nftsite.collectors_site.data.DAO.UserDAO;
 import it.univaq.disim.webengineering.nftsite.collectors_site.data.DAO.WalletDAO;
 import it.univaq.disim.webengineering.nftsite.collectors_site.data.DAOimp.CollectorsDataLayer;
+import it.univaq.disim.webengineering.nftsite.collectors_site.data.model.Collection;
+import it.univaq.disim.webengineering.nftsite.collectors_site.data.model.Nft;
 import it.univaq.disim.webengineering.nftsite.collectors_site.data.model.User;
 import it.univaq.disim.webengineering.nftsite.collectors_site.data.model.Wallet;
 import it.univaq.disim.webengineering.nftsite.framework.data.DataException;
@@ -21,7 +25,6 @@ public class Utility {
     private Utility() {
 
     }
-
 
     public static User getUser(HttpServletRequest request) throws DataException{
         User user;
@@ -37,6 +40,34 @@ public class Utility {
         }
     }
 
+    /**
+     * @return la lista degli utenti i cui dati contengono la keyword passata come parametro nella request
+     */
+    public static List<User> getUsers(HttpServletRequest request) throws DataException {
+        List<User> users;
+        String keyword = request.getParameter("keyword");
+        CollectorsDataLayer dataLayer = ((CollectorsDataLayer) request.getAttribute("datalayer"));
+        UserDAO userDAO = dataLayer.getUserDAO();
+        users = userDAO.getUsersByKeyword(keyword);
+
+        return users;
+    }
+
+    /**
+     * @return la lista delle collezioni i cui dati contengono la keyword passata come parametro nella request
+     */
+    public static List<Collection> getCollections(HttpServletRequest request) throws DataException {
+        List<Collection> collections;
+        String keyword = request.getParameter("keyword");
+        CollectorsDataLayer dataLayer = ((CollectorsDataLayer) request.getAttribute("datalayer"));
+        CollectionDAO collectionDAO = dataLayer.getCollectionDAO();
+        collections = collectionDAO.getCollectionsByKeyword(keyword);
+
+        return collections;
+    }
+
+
+
     public static List<Wallet> getWallets(HttpServletRequest request) throws DataException {
         List<Wallet> wallets;
         HttpSession s = request.getSession(false);
@@ -47,10 +78,20 @@ public class Utility {
             wallets = walletDAO.getWallets(userId);
 
             return wallets;
-        } else {
-            return null;
         }
     }
 
-    public static List<Nft> getNfts()
+    /**
+     * @return la lista dei dischi i cui dati contengono la keyword passata come parametro nella request
+     */
+    public static List<Nft> getNfts(HttpServletRequest request) throws DataException {
+        List<Nft> nfts;
+        String keyword = request.getParameter("keyword");
+        CollectorsDataLayer dataLayer = ((CollectorsDataLayer) request.getAttribute("datalayer"));
+        NftDAO nftDAO = dataLayer.getNftDAO();
+        nfts = nftDAO.getNftByKeyword(keyword);
+
+        return nfts;
+    }
+
 }
