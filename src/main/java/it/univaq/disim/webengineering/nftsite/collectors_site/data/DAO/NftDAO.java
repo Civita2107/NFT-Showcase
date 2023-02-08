27183912f2dc7a61bc405109f75f9fb20f97fb64
FabioMastromauro <mastromaurofabio@gmail.com>
2023-02-08 18:31:51 +0100

    NftDao corretto

--- src/main/java/it/univaq/disim/webengineering/nftsite/collectors_site/data/DAO/NftDAO.java
+++ src/main/java/it/univaq/disim/webengineering/nftsite/collectors_site/data/DAO/NftDAO.java
@@ -20,7 +20,7 @@ public interface NftDAO {
 
     Nft getNft(int key) throws DataException;
 
-    List<Nft> getNft(Comment comment) throws DataException;
+    Nft getNft(Comment comment) throws DataException;
 
     List<Nft> getNftByKeyword(String keyword) throws DataException;
     
