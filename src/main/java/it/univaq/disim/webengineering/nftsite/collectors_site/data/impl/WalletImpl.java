bb5d61547e19d39890bff0de22a6d0945720f2f5
Bepsommer <gtgiorgiot@gmail.com>
2023-02-16 14:03:55 +0100

    aggiornati attributi wallet nelle directory model e imple

--- src/main/java/it/univaq/disim/webengineering/nftsite/collectors_site/data/impl/WalletImpl.java
+++ src/main/java/it/univaq/disim/webengineering/nftsite/collectors_site/data/impl/WalletImpl.java
@@ -1,7 +1,5 @@
 package it.univaq.disim.webengineering.nftsite.collectors_site.data.impl;
 
-import java.sql.Array;
-
 import it.univaq.disim.webengineering.nftsite.collectors_site.data.model.Wallet;
 import it.univaq.disim.webengineering.nftsite.framework.data.DataItemImpl;
 
@@ -9,13 +7,9 @@ import it.univaq.disim.webengineering.nftsite.framework.data.DataItemImpl;
 public class WalletImpl extends DataItemImpl<Integer> implements Wallet  {
 
     private String address;
-    private Array nfts;
     private int userId;
 
     
-
-
-
     public WalletImpl(String address, int userId) {
         super();
         this.address = address;
@@ -33,18 +27,8 @@ public class WalletImpl extends DataItemImpl<Integer> implements Wallet  {
     }
 
     @Override
-    public Array getNfts() {
-        return nfts;
-    }
-
-    @Override
     public void setAddress(String address) {
         this.address = address;
-    }
-
-    @Override
-    public void setNfts(Array nfts) {
-        this.nfts = nfts;
     }
 
     @Override
@@ -57,6 +41,5 @@ public class WalletImpl extends DataItemImpl<Integer> implements Wallet  {
         this.userId = userId;
     }
 
-   
     
 }
--- src/main/java/it/univaq/disim/webengineering/nftsite/collectors_site/data/model/Wallet.java
+++ src/main/java/it/univaq/disim/webengineering/nftsite/collectors_site/data/model/Wallet.java
@@ -1,5 +1,4 @@
 package it.univaq.disim.webengineering.nftsite.collectors_site.data.model;
-import java.sql.Array;
 
 import it.univaq.disim.webengineering.nftsite.framework.data.DataItem;
 
@@ -7,11 +6,7 @@ public interface Wallet extends DataItem<Integer> {
     
     String getAddress();
 
-    Array getNfts();
-
     void setAddress(String d);
-
-    void setNfts(Array nfts);
 
     void setUserId(int id);
     
