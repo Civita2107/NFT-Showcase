f9b2fab8c329de7d097f265c5dbd3ed6ea298d08
Civita2107 <f.civitareale@gmail.com>
2023-02-16 21:54:24 +0100

    NftProxy

--- src/main/java/it/univaq/disim/webengineering/nftsite/collectors_site/data/proxy/NftProxy.java
+++ src/main/java/it/univaq/disim/webengineering/nftsite/collectors_site/data/proxy/NftProxy.java
@@ -1,20 +1,74 @@
 package it.univaq.disim.webengineering.nftsite.collectors_site.data.proxy;
 
+import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+import it.univaq.disim.webengineering.nftsite.collectors_site.data.DAO.WalletDAO;
 import it.univaq.disim.webengineering.nftsite.collectors_site.data.impl.NftImpl;
+import it.univaq.disim.webengineering.nftsite.collectors_site.data.model.Wallet;
+import it.univaq.disim.webengineering.nftsite.framework.data.DataException;
 import it.univaq.disim.webengineering.nftsite.framework.data.DataItemProxy;
 import it.univaq.disim.webengineering.nftsite.framework.data.DataLayer;
 
 public class NftProxy extends NftImpl implements DataItemProxy{
 
     protected boolean modified;
-    protected int key;
+    protected int walletKey;
     protected DataLayer dataLayer;
 
     public NftProxy(DataLayer d) {
         super();
         this.dataLayer = d;
-        this.key = 0;
+        this.walletKey = 0;
         this.modified = false;
+    }
+
+    @Override
+    public void setTitle(String title) {
+        super.setTitle(title);
+        this.modified = true;
+    }
+
+    @Override
+    public void setTokenId(String tokenId) {
+        super.setTokenId(tokenId);
+        this.modified = true;
+    }
+
+    @Override
+    public void setContractAddress(String contractAddress) {
+        super.setContractAddress(contractAddress);
+        this.modified = true;
+    }
+
+    @Override
+    public void setDescription(String description) {
+        super.setDescription(description);
+        this.modified = true;
+    }
+
+    @Override
+    public void setMetadata(String metadata) {
+        super.setMetadata(metadata);
+        this.modified = true;
+    }
+
+    @Override
+    public void setWalletAddress(String walletAddress) {
+        super.setMetadata(walletAddress);
+        this.modified = true;
+    }
+
+    @Override
+    public String getWalletAddress() {
+        if (super.getWalletAddress() == null) {
+            try {
+                super.setWalletAddress(((WalletDAO) dataLayer.getDAO(Wallet.class)).getWallet(wallet_key));
+            } catch(DataException e) {
+                Logger.getLogger(WalletProxy.class.getName()).log(Level.SEVERE, null, e);
+            }
+        }
     }
 
     @Override
