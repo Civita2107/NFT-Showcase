f9746e880db12477b754a00b2fd9bc6aba73bed3
Civita2107 <f.civitareale@gmail.com>
2023-02-28 18:56:06 +0100

    Aggiunto controllo se l'utente è loggato nella sezione commenti e controllo se la collezione è pubblica o privata

--- src/main/java/it/univaq/disim/webengineering/nftsite/collectors_site/controller/user/VisualizzaUtente.java
+++ src/main/java/it/univaq/disim/webengineering/nftsite/collectors_site/controller/user/VisualizzaUtente.java
@@ -12,6 +12,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import it.univaq.disim.webengineering.nftsite.collectors_site.controller.CollectorsBaseController;
+import it.univaq.disim.webengineering.nftsite.collectors_site.controller.Utility;
 import it.univaq.disim.webengineering.nftsite.collectors_site.data.DAO.UserDAO;
 import it.univaq.disim.webengineering.nftsite.collectors_site.data.DAOimp.CollectorsDataLayer;
 import it.univaq.disim.webengineering.nftsite.collectors_site.data.model.Collection;
@@ -50,12 +51,26 @@ public class VisualizzaUtente extends CollectorsBaseController {
         CollectorsDataLayer dataLayer = ((CollectorsDataLayer) request.getAttribute("datalayer"));
         UserDAO userDAO = dataLayer.getUserDAO();
         user = userDAO.getUser((Integer.parseInt(request.getParameter("id"))));
-
+        Set<Collection> collezioni1 = new HashSet<>(); 
         Set<Collection> collezioni = new HashSet<>(dataLayer.getCollectionDAO().getCollections(user));
         List<Nft> nfts = dataLayer.getNftDAO().getNfts(user);
 
+        try {
+            if (!request.getParameter("id").equals(String.valueOf(Utility.getUser(request).getKey()))) {
+                for (Collection collection : collezioni) {
+                    if (collection.isPubblica()) {
+                        collezioni1.add(collection);
+                    }                    
+                }
+            } else {
+                collezioni1 = new HashSet<>(dataLayer.getCollectionDAO().getCollections(user));
+            }
+        } catch (NullPointerException e) {
+            //
+        }
+
         request.setAttribute("user", user);
-        request.setAttribute("collezioni", collezioni);
+        request.setAttribute("collezioni", collezioni1);
         request.setAttribute("nfts", nfts);
         result.activate("user/visualizza.ftl", request, response);
     }
